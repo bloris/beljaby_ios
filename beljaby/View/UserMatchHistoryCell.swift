5b46f13c93c39ee5a5d1892e2fd28659cc1f658c
Hoyoun Lee <hy2930@gmail.com>
2022-07-19 16:36:56 +0900

    User Match History UI V0.1

--- beljaby/View/UserMatchHistoryCell.swift
+++ beljaby/View/UserMatchHistoryCell.swift
@@ -7,17 +7,11 @@
 
 import UIKit
 
-class UserMatchHistoryCell: UITableViewCell {
+class UserMatchHistoryCell: UICollectionViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 }
